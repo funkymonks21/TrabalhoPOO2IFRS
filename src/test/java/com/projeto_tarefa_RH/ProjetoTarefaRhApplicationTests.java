6ef091a913b53b1ffa5e18437073de5edc01f553
Giordano Debenedetti <83867955+funkymonks21@users.noreply.github.com>
2025-11-03 20:39:32 -0300

    add files via upload

---
+++ src/test/java/com/projeto_tarefa_RH/ProjetoTarefaRhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto_tarefa_RH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoTarefaRhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
