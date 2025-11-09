e8675c0a39cf01ec620031c1164ca2a84d4603b1
PedroCaspar <caspar.pedro@gmail.com>
2025-11-09 15:15:25 -0300

    Create Cargo entity with properties and methods

---
+++ src/main/java/com.projeto_rh/controller/Cargo.java
@@ -0,0 +1,38 @@
+package com.projeto_rh.controller;
+
+import org.hibernate.annotations.DynamicUpdate;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+@DynamicUpdate
+public class Cargo{
+	@Id
+	@GeneratedValue
+	private String nome;
+	private String descricao;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public String toString() {
+		return 	"Cargo [Nome = " + nome +
+				", Descrição = " + descricao +
+				"]";
+	}	
+}
