4013a767fcf53ec76016aab6820ceb32d2dec0cb
PedroCaspar <caspar.pedro@gmail.com>
2025-11-10 21:10:49 -0300

    Convert FuncionarioRepository to a Spring Data repository

--- src/main/java/com/empresa/rh/repository/FuncionarioRepository.java
+++ src/main/java/com/empresa/rh/repository/FuncionarioRepository.java
@@ -1,5 +1,11 @@
 package com.empresa.rh.repository;
 
-public class FuncionarioRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.empresa.rh.model.entity.Funcionario;
+
+@Repository
+public interface FuncionarioRepository extends JpaRepository<Funcionario,Long>{
 
 }
