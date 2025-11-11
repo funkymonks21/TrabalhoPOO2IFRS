0a016296ab8fa4ba934f3c5a940ee864696d3b1e
Giordano Debenedetti <83867955+funkymonks21@users.noreply.github.com>
2025-11-10 21:43:33 -0300

    Refactor DepartamentoRepository and add existsByNome method
    Added method to check existence of Departamento by name.

--- src/main/java/com/empresa/rh/repository/DepartamentoRepository.java
+++ src/main/java/com/empresa/rh/repository/DepartamentoRepository.java
@@ -1,12 +1,10 @@
-package com.empresa.rh.repository;
+package com.empresa.rh.model.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.empresa.rh.model.entity.Departamento;
 
 @Repository
-public interface DepartamentoRepository extends JpaRepository<Departamento,Long>{
-
+public interface DepartamentoRepository extends JpaRepository<Departamento, Long> {
+    boolean existsByNome(String nome);
 }
-
