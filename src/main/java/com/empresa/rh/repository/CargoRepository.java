38f5ac23557dea86d365ee3d65354ab0f7fd87f3
PedroCaspar <caspar.pedro@gmail.com>
2025-11-10 21:11:38 -0300

    Refactor CargoRepository to use JpaRepository

--- src/main/java/com/empresa/rh/repository/CargoRepository.java
+++ src/main/java/com/empresa/rh/repository/CargoRepository.java
@@ -1,5 +1,12 @@
 package com.empresa.rh.repository;
 
-public class CargoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.empresa.rh.model.entity.Cargo;
+
+@Repository
+public interface CargoRepository extends JpaRepository<Cargo,String>{
 
 }
+
