e3f62cfb8b8b20e7af2a5a4ead02e23e351dbac1
PedroCaspar <caspar.pedro@gmail.com>
2025-11-10 21:11:15 -0300

    Convert DepartamentoRepository to a Spring Data JPA interface

--- src/main/java/com/empresa/rh/repository/DepartamentoRepository.java
+++ src/main/java/com/empresa/rh/repository/DepartamentoRepository.java
@@ -1,5 +1,12 @@
 package com.empresa.rh.repository;
 
-public class DepartamentoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.empresa.rh.model.entity.Departamento;
+
+@Repository
+public interface DepartamentoRepository extends JpaRepository<Departamento,Long>{
 
 }
+
