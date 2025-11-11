1036fd1973e279e2d6eaed2c78b4daa5962464d6
Giordano Debenedetti <83867955+funkymonks21@users.noreply.github.com>
2025-11-10 22:00:07 -0300

    Refactor FuncionarioRepository to use CPF methods

--- src/main/java/com/empresa/rh/repository/FuncionarioRepository.java
+++ src/main/java/com/empresa/rh/repository/FuncionarioRepository.java
@@ -1,10 +1,11 @@
 package com.empresa.rh.model.repository;
 
+import com.empresa.rh.model.entity.Funcionario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.empresa.rh.model.entity.Funcionario;
 
 @Repository
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
-    boolean existsByEmail(String email);
+    boolean existsByCpf(String cpf);
+    Funcionario findByCpf(String cpf);
 }
