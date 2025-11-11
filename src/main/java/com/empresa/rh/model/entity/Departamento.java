e10f493aeb89b85423942a2a27e299717d88d78e
PedroCaspar <caspar.pedro@gmail.com>
2025-11-10 21:13:38 -0300

    Refactor Funcionario class into Departamento class

--- src/main/java/com/empresa/rh/model/entity/Departamento.java
+++ src/main/java/com/empresa/rh/model/entity/Departamento.java
@@ -1,91 +1,49 @@
 package com.empresa.rh.model.entity;
 
-import java.util.Calendar;
 import org.hibernate.annotations.DynamicUpdate;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
 
 @Entity
 @DynamicUpdate
-public class Funcionario {
-   @Id
-   @GeneratedValue
-   private Long id;
-   private String nome;
-   private String email;
-   @Temporal(TemporalType.DATE)
-   private Calendar data_contratacao;
-   private double salario;
-   private Cargo cargo;
-   private Funcionario chefe;
-    
-   public Long getId() {
-	   return id;
-   }
+public class Departamento {
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String nome;
+	private String descricao;
+	
+	public Long getId() {
+		return id;
+	}
 
-   public void setId(Long id) {
-	   this.id = id;
-   }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-   public String getNome() {
-	   return nome;
-   }
+	public String getNome() {
+		return nome;
+	}
 
-   public void setNome(String nome) {
-	   this.nome = nome;
-   }
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
-   public String getEmail() {
-	   return email;
-   }
+	public String getDescricao() {
+		return descricao;
+	}
 
-   public void setEmail(String email) {
-	   this.email = email;
-   }
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
 
-   public Calendar getData_contratacao() {
-	   return data_contratacao;
-   }
-
-   public void setData_contratacao(Calendar data_contratacao) {
-	   this.data_contratacao = data_contratacao;
-   }
-
-   public double getSalario() {
-	   return salario;
-   }
-
-   public void setSalario(double salario) {
-	   this.salario = salario;
-   }
-   
-   public Cargo getCargo() {
-	   return cargo;
-   }
-
-   public void setCargo(Cargo cargo) {
-	   this.cargo = cargo;
-   }
-
-   public Funcionario getChefe() {
-	   return chefe;
-   }
-
-   public void setChefe(Funcionario chefe) {
-	   this.chefe = chefe;
-   }
-
-   @Override
-   public String toString() {
-	   return 	"Funcionario [id = " + id +
-				", nome = " + nome +
-				", email = " + email +
-				", data_contratacao = " + data_contratacao +
-				", salario = " + salario +
-				"]";
-   }	
+	@Override
+	public String toString() {
+		return 
+			"Departamento [id = " + id +
+			", Nome = " + nome +
+			", Descrição  = " + descricao +
+			"]";
+	}	
 }
-
