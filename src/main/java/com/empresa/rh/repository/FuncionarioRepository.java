3665b275660a080dcd64586b078b0f5e14f9d7cf
Giordano Debenedetti <83867955+funkymonks21@users.noreply.github.com>
2025-11-10 21:47:41 -0300

    Enhance FuncionarioRepository with email check
    Added method to check if a Funcionario exists by email.

--- src/main/java/com/empresa/rh/repository/FuncionarioRepository.java
+++ src/main/java/com/empresa/rh/repository/FuncionarioRepository.java
@@ -1,11 +1,10 @@
-package com.empresa.rh.repository;
+package com.empresa.rh.model.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.empresa.rh.model.entity.Funcionario;
 
 @Repository
-public interface FuncionarioRepository extends JpaRepository<Funcionario,Long>{
-
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
+    boolean existsByEmail(String email);
 }
